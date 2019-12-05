3bf41c6a54a73ab4e594064ca820cbfc24361a2d
John Beaderstadt <jmbeaderstadt@gmail.com>
2019-12-05 11:20:49 -0600

    Added: Starting player is randomized, clicking cells switches players & assigns appropriate marker

--- js/scripts.js
+++ js/scripts.js
@@ -2,15 +2,52 @@
 //Game Board module
 //Manipulation of the game board
 const gameBoard = (() => {
-    let board = ["X", "O", "X", "X", "O", "X", "X", "O", "X"];
+    let board = [];
 
-    return { board }
+    const drawBoard = function() {
+        const container = document.getElementById("container");
+        for (i = 0; i < 9; i++) {
+            let div = document.createElement('div');
+            div.setAttribute("quadrant", [i])
+            container.append(div)
+        }
+    }
+
+    const render = function() {
+        let index = document.querySelectorAll("[quadrant]");
+        for (i = 0; i < index.length; i++) {
+            let marker = gameBoard.board[i]
+            if (!marker) continue;
+            index[i].innerHTML = marker;
+            console.log(marker);
+        }
+    }
+
+    const drawMarker = function() {
+        let index = this.getAttribute("quadrant");
+        console.log(index)
+        if (!gameBoard.board[index]) gameBoard.board[index] = player1.getMarker();
+        render();
+    }
+
+    return { board, drawBoard, render, drawMarker }
 })();
 
 //Game flow module
 //Switching players, checking for winner, keeping score
-const game = (() => {
+// Logic flow: Randomly decide first player (state variable for current player)
+// Cell is clicked and:
+// 1) Checks to see if index is empty, then renders the current players marker
+// 2) Adds marker to array
+// 3) Check for winner (function)
+// 4) Switch players (and the subsequent marker)
 
+const game = (() => {
+    let currentPlayer = player1;
+
+    return { currentPlayer }
+
+    // const checkWinner;
 })
 
 //Player factories
@@ -26,21 +63,21 @@ const newPlayer = (player, marker) => {
 const player1 = newPlayer("1", "X")
 const player2 = newPlayer("2", "O")
 
-//Initialization of gameboard/HTML render
-function drawBoard() {
-    const container = document.getElementById("container");
-    for (i = 0; i < 9; i++) {
-        let div = document.createElement('div');
-        div.setAttribute("quadrant", [i])
-        container.append(div)
-    }
-}
 
-//Rendering
-function render() {
 
-}
 
 //Testing area
 
-drawBoard();
+gameBoard.drawBoard();
+gameBoard.render();
+
+
+
+
+
+
+
+
+//Event Listeners
+const cells = Array.from(document.querySelectorAll("[quadrant]"));
+cells.forEach(cell => cell.addEventListener('click', gameBoard.drawMarker));
